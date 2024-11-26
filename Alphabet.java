6b1e646671a67136429c35d56ad2692afa1675fc
ritik kumar <ritik31dr@gmail.com>
2024-11-26 23:20:07 +0530

    Update submodule

--- Alphabet.java
+++ Alphabet.java
@@ -2,7 +2,7 @@ package pattern;
 
 public class Alphabet {
     public static void main(String[] args) {
-        //A
+        //!A
 
         // int n = 5;
         // int m = 9;
@@ -18,7 +18,7 @@ public class Alphabet {
         // }
 
 
-        //B
+        //!B
 
         // int n = 7;
         // for (int i = 1; i <= n; i++) {
@@ -33,7 +33,7 @@ public class Alphabet {
         // }
 
 
-        //C
+        //!C
 
         // int n = 7;
         // for (int i = 1; i <= n; i++) {
@@ -48,7 +48,7 @@ public class Alphabet {
         // }
 
 
-        // D
+        //!D
 
         // int n = 7;
         // for (int i = 1;i <= n; i++) {
@@ -63,7 +63,7 @@ public class Alphabet {
         // }
 
 
-        //E
+        //!E
 
         // int n = 7;
         // for (int i = 1;i <= n; i++) {
@@ -78,7 +78,7 @@ public class Alphabet {
         // }
 
 
-        //F
+        //!F
 
         // int n = 7;
         // for (int i = 1;i <= n; i++) {
@@ -93,7 +93,7 @@ public class Alphabet {
         // }
 
 
-        //G
+        //!G
 
         // int n = 7;
         // for (int i = 1;i <= n; i++) {
@@ -108,7 +108,7 @@ public class Alphabet {
         // }
 
 
-        //H
+        //!H
 
         // int n = 7;
         // for (int i = 1;i <= n; i++) {
@@ -123,7 +123,7 @@ public class Alphabet {
         // }
 
 
-        //I
+        //!I
 
         // int n = 7;
         // for (int i = 1;i <= n; i++) {
@@ -138,7 +138,7 @@ public class Alphabet {
         // }
 
 
-        //J
+        //!J
 
         // int n = 7;
         // for (int i = 1;i <= n; i++) {
@@ -153,19 +153,229 @@ public class Alphabet {
         // }
 
 
-        //K
+        //!K
 
-        int n = 7;
-        int m = 4;
-        for (int i = 1;i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (j == 1 || j == 6-i || i == 2+j) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (j == 1 || j == 6-i || i == 2+j) {
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        //!L
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (j == 1 || i == n) {
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        //!M
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (j == 1 || j == n || (i == j && j <= 4) || (j == 8 - i && j > 4)) {
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        //!N
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (j == 1 || j == n || i == j) {
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        //!O
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (i == 1 || j == 1 || j == n || i == n) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!P
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (i == 1 || j == 1 || (j == n && i < 4) || i == 4) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!R
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (i == 1 || j == 1 || (j == n && i < 4) || i == 4 || (i - j == 2 && i > 4)) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!S
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (i == 1 || (j == 1 && i < 4) || (j == n && i > 4) || i == 4 || i == n) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!T
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (i == 1 || j == 4) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!U
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (j == 1 || j == n || i == n) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!V
+
+        // int n = 5;
+        // int m = 9;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= m; j++) {
+        //     if (i == j || (j == (m + 1) - i)) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!w
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (j == 1 || (i == j && i >= 4) || (j == (n + 1) - i && i > 4) || j == n) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!X
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (i == j || j == (n + 1) - i) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!Y
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if ((i == j && j <= 4) || (j == (n + 1) - i && i < 4) || (j == 4 && i > 4)) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
+
+
+        //!Z
+
+        // int n = 7;
+        // for (int i = 1;i <= n; i++) {
+        //   for (int j = 1; j <= n; j++) {
+        //     if (i == 1 || (j == (n + 1) - i) || i == n) {
+        //       System.out.print("*");
+        //     } else {
+        //       System.out.print(" ");
+        //     }
+        //   }
+        //   System.out.println();
+        // }
     }
 }
